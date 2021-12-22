bfb986fd2da0663ad3a52c878bc1269a7ac8a309
Ilya Pakhomov <ipakhomov@ronasit.com>
2021-12-23 00:01:20 +0600

    Setup basic app

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# react-native-starter
-Ronas IT starter repository for React Native + Expo projects
+# Driven - React Native
+
+## Development
+
+1. Setup [Expo](https://docs.expo.io/get-started/installation/#installing-expo-cli)
+1. Run expo server: `npm run start`
+1. Open your app using [Expo Client](https://docs.expo.io/get-started/installation/#2-mobile-app-expo-client-for-ios)
+
+See `package.json` for platform specific run/build commands.
---
+++ src/app/index.tsx
@@ -0,0 +1,40 @@
+import 'reflect-metadata';
+
+import { configuration } from '@configurations';
+import { store } from '@store';
+import { registerRootComponent } from 'expo';
+import { useFonts } from 'expo-font';
+import React, { ReactElement } from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Provider } from 'react-redux';
+import * as Sentry from 'sentry-expo';
+import { App } from './app';
+
+export default function Root(): ReactElement {
+  Sentry.init({
+    dsn: configuration.sentry.dsn,
+    enableInExpoDevelopment: false,
+    debug: !configuration.production
+  });
+
+  const [isFontsLoaded] = useFonts({
+    SFProDisplayBold: require('@assets/fonts/SF-Pro-Display-Bold.otf'),
+    SFProDisplayRegular: require('@assets/fonts/SF-Pro-Display-Regular.otf'),
+    SFProTextRegular: require('@assets/fonts/SF-Pro-Text-Regular.otf'),
+    SFProTextSemiBold: require('@assets/fonts/SF-Pro-Text-Semibold.otf')
+  });
+
+  if (!isFontsLoaded) {
+    return null;
+  }
+
+  return (
+    <SafeAreaProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </SafeAreaProvider>
+  );
+}
+
+registerRootComponent(Root);
