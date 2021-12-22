15eaa53061417c3565601de0c17d029e3af9807a
Ilya Pakhomov <ipakhomov@ronasit.com>
2021-12-23 00:03:49 +0600

    Add assets

---
+++ src/assets/icons/icons.tsx
@@ -0,0 +1,11 @@
+import { Svg } from '@shared/svg';
+
+/* eslint-disable @typescript-eslint/no-var-requires */
+export const Icons = {
+  eye: Svg(require('./eye.svg')),
+  eyeHide: Svg(require('./eye-hide.svg')),
+  user: Svg(require('./user.svg')),
+  home: Svg(require('./home.svg')),
+  check: Svg(require('./check.svg')),
+  chevronDown: Svg(require('./chevron-down.svg'))
+};
