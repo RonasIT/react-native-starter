e38523406d55b6639daf93a7605ef50e46f8dbae
Ilya Pakhomov <ipakhomov@ronasit.com>
2021-12-24 20:27:46 +0600

    Form group component

---
+++ src/app/shared/form-group/component.tsx
@@ -0,0 +1,54 @@
+import { AppText, TextTheme } from '@shared/text';
+import { AppTextInputProps } from '@shared/text-input';
+import { createStyles } from '@styles';
+import { FormikProps } from 'formik';
+import React, { ReactElement, useEffect } from 'react';
+import { View, ViewStyle } from 'react-native';
+
+export interface FormGroupProps<T extends Record<string, any> = Record<string, any>>
+  extends Partial<Pick<FormikProps<T>, 'errors' | 'touched'>> {
+  label?: string;
+  name: keyof T;
+  isSubmitted?: boolean;
+  containerStyle?: ViewStyle;
+  children?: ReactElement;
+  onErrorStateChange?: (hasError: boolean) => void;
+}
+
+export function FormGroup<T = AppTextInputProps>({
+  errors,
+  name,
+  label,
+  isSubmitted,
+  touched,
+  containerStyle,
+  children,
+  onErrorStateChange
+}: FormGroupProps & T): ReactElement {
+  const hasError = !!errors?.[name] && (isSubmitted || !!touched?.[name]);
+
+  useEffect(() => {
+    onErrorStateChange?.(hasError);
+  }, [hasError]);
+
+  return (
+    <View style={[style.formGroup, containerStyle]}>
+      {!!label && (
+        <AppText theme={TextTheme.LARGER} style={style.formGroupLabel}>
+          {label}
+        </AppText>
+      )}
+      {children}
+      <AppText>{errors[name] || ''}</AppText>
+    </View>
+  );
+}
+
+const style = createStyles({
+  formGroup: {
+    marginBottom: '1.2rem'
+  },
+  formGroupLabel: {
+    marginBottom: '0.5rem'
+  }
+});
