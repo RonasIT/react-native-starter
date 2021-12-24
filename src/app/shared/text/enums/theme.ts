75fcbf8af3a810b3b3494dd7ce5890d9d3497e9a
Ilya Pakhomov <ipakhomov@ronasit.com>
2021-12-24 19:19:55 +0600

    Setup text component

---
+++ src/app/shared/text/enums/theme.ts
@@ -0,0 +1,8 @@
+export enum TextTheme {
+  SMALLEST = 'textSmallest',
+  SMALL = 'textSmall',
+  MEDIUM = 'textMedium',
+  LARGER = 'textLarger',
+  LARGE = 'textLarge',
+  LARGEST = 'textLargest'
+}
