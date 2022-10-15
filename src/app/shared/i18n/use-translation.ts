24ed701bdd9bccb407bb50172bd6145f5ab41334
Vasily Eliseev <veliseev@ronasit.com>
2022-10-15 16:52:41 +0600

    chore: upgrade i18n-js library version

--- src/app/shared/i18n/use-translation.ts
+++ src/app/shared/i18n/use-translation.ts
@@ -1,7 +1,8 @@
-import I18n, { TranslateOptions } from 'i18n-js';
+import { TranslateOptions } from 'i18n-js';
+import { i18n } from './i18n';
 
 export function useTranslation(basePath: string) {
   return (key: string, options?: TranslateOptions): string => {
-    return I18n.t(`${basePath}.${key}`, options);
+    return i18n.t(`${basePath}.${key}`, options);
   };
 }
