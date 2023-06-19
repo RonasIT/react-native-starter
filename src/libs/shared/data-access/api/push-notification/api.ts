7060e66e8a4c31e463009daa1af39621c121e35f
ekazankova <ekazankova@ronasit.com>
2023-06-19 09:33:19 +0600

    Used axiosBaseQuery in pushNotificationAPI

--- src/libs/shared/data-access/api/push-notification/api.ts
+++ src/libs/shared/data-access/api/push-notification/api.ts
@@ -1,11 +1,11 @@
 import { retry } from '@reduxjs/toolkit/query';
 import { instanceToPlain } from 'class-transformer';
-import { axiosBaseQueryWithTokenRefresh, createAppApi } from '@libs/shared/data-access/api-client';
+import { axiosBaseQuery, createAppApi } from '@libs/shared/data-access/api-client';
 import { PushNotificationSubscriptionRequest } from './models';
 
 export const pushNotificationAPI = createAppApi({
   reducerPath: 'push-notification',
-  baseQuery: retry(axiosBaseQueryWithTokenRefresh),
+  baseQuery: retry(axiosBaseQuery),
   endpoints: (builder) => ({
     subscribeDevice: builder.mutation<void, PushNotificationSubscriptionRequest>({
       query: (params) => {
